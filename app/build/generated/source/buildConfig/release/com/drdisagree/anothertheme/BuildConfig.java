f65f3a16597cc4f160104d6aa88d0deadcc490a8
DrDisagree <crazymahmud08@gmail.com>
2022-08-18 12:01:31 -0700

    Project commit

--- app/build/generated/source/buildConfig/release/com/drdisagree/anothertheme/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/drdisagree/anothertheme/BuildConfig.java
@@ -7,8 +7,8 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.drdisagree.anothertheme";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 10;
-  public static final String VERSION_NAME = "1.9";
+  public static final int VERSION_CODE = 11;
+  public static final String VERSION_NAME = "2.0";
   // Field from default config.
   public static final boolean ALLOW_THIRD_PARTY_SUBSTRATUM_BUILDS = true;
   // Field from build type: release
@@ -16,13 +16,13 @@ public final class BuildConfig {
   // Field from build type: release
   public static final String BASE_64_LICENSE_KEY = "";
   // Field from default config.
-  public static final byte[] DECRYPTION_KEY = {-123, 34, -70, 106, -100, -48, 24, -122, -116, 97, 124, -103, -89, 87, -102, -48};
+  public static final byte[] DECRYPTION_KEY = {85, 62, -121, -116, -15, 70, 115, -45, 105, 79, 63, -75, -21, 120, -116, 115};
   // Field from default config.
   public static final boolean ENABLE_APP_BLACKLIST_CHECK = true;
   // Field from build type: release
   public static final boolean ENFORCE_GOOGLE_PLAY_INSTALL = false;
   // Field from default config.
-  public static final byte[] IV_KEY = {-123, -34, 81, 48, 22, -31, 22, -52, -26, -21, 39, -121, 6, -2, -12, 8};
+  public static final byte[] IV_KEY = {35, -53, 71, 60, -15, -19, 10, 85, -104, 0, 108, -117, -116, 110, -17, 10};
   // Field from default config.
   public static final boolean SUPPORTS_THIRD_PARTY_SYSTEMS = true;
 }
